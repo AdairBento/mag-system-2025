a146db2bd0dce9d8f803b0f2385bac738a5b6745
AdairBento <adair.bento@hotmail.com>
2026-01-15 09:40:23 -0300

    feat(api): registra VehiclesModule no AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [ClientsModule, VehiclesModule],
 })
 export class AppModule {}
